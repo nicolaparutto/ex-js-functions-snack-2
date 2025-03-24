b6d574845a29166f03432198efcfb171e9c7091e
nicolaparutto <nicolaparutto2003@gmail.com>
2025-03-24 16:31:45 +0100

    build: add exercise files

---
+++ snack.js
@@ -0,0 +1,62 @@
+// 🏆 Snack 1
+// Crea una funzione che somma due numeri.
+// ✅ Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+// ✅ Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile.
+// ✅ Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+
+
+
+// 🏆 Snack 2
+// Crea una arrow function che calcola il quadrato di un numero.
+// ✅ Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+
+
+// 🏆 Snack 3
+// Crea una funzione eseguiOperazione.
+// ✅ Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+
+
+// 🏆 Snack 4
+// Crea un generatore di funzioni creaTimer.
+// ✅ Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione. La nuova funzione deve avviare un setTimeout per stampare "Tempo scaduto!".
+
+
+
+// 🏆 Snack 5
+// Crea una funzione stampaOgniSecondo con setInterval.
+// ✅ Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+// 🔔 Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+
+
+
+// 🏆 Snack 6
+// Crea un contatore automatico con setInterval.
+// ✅ Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo. La funzione restituisce una nuova funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+
+
+
+// 🏆 Snack 7
+// Crea una funzione che ferma un timer dopo un certo tempo.
+// ✅ Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+
+
+
+// 🏆 Snack 8 (BONUS)
+// Crea una funzione che simula un conto alla rovescia.
+// ✅ Scrivi una funzione contoAllaRovescia che accetta un numero n e stampa il conto alla rovescia da n a 0, con un intervallo di 1 secondo tra ogni numero. Quando arriva a 0, stampa "Tempo scaduto!" e interrompe il timer.
+
+
+
+// 🏆 Snack 9 (BONUS)
+// Creare una funzione che esegue una sequenza di operazioni con ritardi.
+// ✅ Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+// Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
+
+
+
+// 🏆 Snack 10 (BONUS)
+// Creare un throttler per limitare l’esecuzione di una funzione.
+// ✅ Scrivi una funzione creaThrottler che accetta una funzione e  un tempo `limite`.
+// Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
